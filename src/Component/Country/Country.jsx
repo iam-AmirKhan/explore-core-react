41d26f06552240cd10becdf35346bd89f41f5d59
Amir Khan <khansourav119@gmail.com>
2025-09-19 22:12:46 +0600

    3d push

--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
-    console.log(country)
+    // console.log(country.population.population)
     return (
-       <div>
-    <h3>Name: </h3>
+       <div className='country'>
+        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+    <h3>Name: {country.name.common}</h3>
+    <p>population: {country.population.population}</p>
     </div>
     );
 };
