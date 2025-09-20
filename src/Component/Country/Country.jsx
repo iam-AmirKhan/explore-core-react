043f74e7c508206ad6acd6eada017898354494f9
Amir Khan <khansourav119@gmail.com>
2025-09-20 21:42:03 +0600

    5d push

--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import './Country.css'
+import React, { useState } from 'react';
+import './Country.css';
 
-const Country = ({country}) => {
-    console.log(country.area.area)
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
+  const [visited, setVisited] = useState(false);
+  console.log(handleVisitedCountries);
 
-    const handleVisited = () =>{
-        console.log ('button clicked')
-    } 
-    return (
-       <div className='country'>
-        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-    <h3>Name: {country.name.common}</h3>
-    <p>population: {country.population.population}</p>
-    <p>Area: {country.area.area} {
-        country.area.area > 300000 ? 'Big Country' : 'Small country'
-        }</p>
-        <button onClick={handleVisited}>Not Visited</button>
+  const handleVisited = () => {
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
+
+  return (
+    <div className='country'>
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+       <p>Population: {country.population.population}</p>
+      <p>
+        {/* Area: {country.area} {country.area > 300000 ? 'Big Country' : 'Small Country'} */}
+      </p>
+      <button className='bg-amber-500' onClick={handleVisited}>
+        {visited ? 'Visited' : 'Not Visited'}
+      </button>
+      <button className='bg-green-500' onClick={()=>{ handleVisitedFlag(country?.flags?.flags?.png)}}>Add Flag</button>
     </div>
-        
-    );
+  );
 };
 
 export default Country;
